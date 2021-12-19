5694328d1061e0737c7e87710de419c4a112ce21
galamo <galamouyal88@gmail.com>
2021-12-19 21:18:13 +0200

    object_with_function

--- objects_in_js/index.js
+++ objects_in_js/index.js
@@ -1,2 +1,19 @@
 // use {}
 const user = {};
+
+// use Object create
+const userByObj = Object.create({ userName: "Gal" });
+
+// using __proto__ to access the main JS obect.
+
+function Product(category, price, name) {
+  this.Category = category;
+  this.Price = price;
+  this.Name = name;
+}
+
+Product.prototype.printProduct = function () {
+  console.log(`${this.Name} => ${this.Category}`);
+};
+
+const product = new Product("drinks", 12, "beer");
