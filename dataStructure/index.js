86b3dab3efad64b0dd952106f6810deb6c18124c
galamo <galamouyal88@gmail.com>
2021-12-22 21:28:41 +0200

    dynamic list

--- dataStructure/index.js
+++ dataStructure/index.js
@@ -19,28 +19,34 @@
 // Object
 // { user:gal } => obj[user]
 
-const carsTypes = ["mazda", "kia", "bmw", "rangerover", "ferari"];
-const colors = ["red", "pink", "yellow", "white", "black"];
-const engines = [1200, 1400, 1500, 1600, 2000];
+const state = {
+  types: ["mazda", "kia", "bmw", "rangerover", "ferari"],
+  colors: ["red", "pink", "yellow", "white", "black"],
+  engines: [1200, 1400, 1500, 1600, 2000],
+  doors: [2, 3, 4, 5],
+};
 
 const carsArray = [];
 const carsObj = {};
+const { types, colors, engines, doors } = state;
 function generateCars() {
   for (let index = 0; index < 999999; index++) {
-    const type = carsTypes[Math.floor(Math.random() * 5)];
+    const type = types[Math.floor(Math.random() * 5)];
     const color = colors[Math.floor(Math.random() * 5)];
     const engine = engines[Math.floor(Math.random() * 5)];
-    const currentCar = new Car(type, color, engine);
+    const numberOfdoors = doors[Math.floor(Math.random() * 4)];
+    const currentCar = new Car(type, color, engine, numberOfdoors);
     carsArray.push(currentCar);
     carsObj[currentCar.lp] = currentCar;
   }
 }
 
-function Car(_type, _color, _engine) {
+function Car(_type, _color, _engine, _doors) {
   this.type = _type;
   this.lp = generateLP();
   this.color = _color;
   this.engine = _engine;
+  this.numberOfDoors = _doors;
 }
 
 function generateLP() {
@@ -66,4 +72,33 @@ function searchCarObj() {
   console.log(result);
 }
 
+function init() {
+  const keySelectOption = document.querySelector("#searchBy");
+  const options = Object.keys(state).map(function (prop) {
+    return _getOptionElement(prop);
+  });
+
+  keySelectOption.append(...options);
+  selectKey();
+}
+
+function selectKey() {
+  const keySelectOption = document.querySelector("#searchBy");
+  const valuesSelectOption = document.querySelector("#searchValue");
+  const arrayOfOptions = state[keySelectOption.value];
+  const options = arrayOfOptions.map(function (value) {
+    return _getOptionElement(value);
+  });
+  valuesSelectOption.innerHTML = "";
+  valuesSelectOption.append(...options);
+}
+
+function _getOptionElement(prop) {
+  const option = document.createElement("option");
+  option.value = prop;
+  option.innerText = prop;
+  return option;
+}
+
+init();
 generateCars();
