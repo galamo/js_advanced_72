8ec4c5c7305510ba6a6e77d081231bf2b1997574
TalBest <talbest94@gmail.com>
2021-12-25 17:29:14 +0200

    trid to use applay search via the object of cars not sure this is the right way

--- dataStructure/index.js
+++ dataStructure/index.js
@@ -24,29 +24,32 @@ const state = {
   colors: ["red", "pink", "yellow", "white", "black"],
   engines: [1200, 1400, 1500, 1600, 2000],
   doors: [2, 3, 4, 5],
+  ranks: ["a", "b", "c", "d"]
 };
 
 const carsArray = [];
 const carsObj = {};
-const { types, colors, engines, doors } = state;
+const { types, colors, engines, doors, ranks } = state;
 function generateCars() {
-  for (let index = 0; index < 999999; index++) {
+  for (let index = 0; index < 99; index++) {
     const type = types[Math.floor(Math.random() * 5)];
     const color = colors[Math.floor(Math.random() * 5)];
     const engine = engines[Math.floor(Math.random() * 5)];
     const numberOfdoors = doors[Math.floor(Math.random() * 4)];
-    const currentCar = new Car(type, color, engine, numberOfdoors);
+    const rankofCar = ranks[Math.floor(Math.random() * 4)];
+    const currentCar = new Car(type, color, engine, numberOfdoors, rankofCar);
     carsArray.push(currentCar);
     carsObj[currentCar.lp] = currentCar;
   }
 }
 
-function Car(_type, _color, _engine, _doors) {
+function Car(_type, _color, _engine, _doors, _rank) {
   this.type = _type;
   this.lp = generateLP();
   this.color = _color;
   this.engine = _engine;
-  this.numberOfDoors = _doors;
+  this.door = _doors;
+  this.rank = _rank
 }
 
 function generateLP() {
@@ -74,8 +77,11 @@ function searchCarObj() {
 
 function init() {
   const keySelectOption = document.querySelector("#searchBy");
+  const findBtn = document.querySelector("#searchNew");
+  findBtn.addEventListener("click", newSearch)
   const options = Object.keys(state).map(function (prop) {
     return _getOptionElement(prop);
+
   });
 
   keySelectOption.append(...options);
@@ -98,6 +104,17 @@ function _getOptionElement(prop) {
   option.value = prop;
   option.innerText = prop;
   return option;
+}
+
+function newSearch() {
+  const type = document.querySelector("#searchBy").value;
+  const fixtype = type.toString().slice(0, -1);
+  const innerType = document.querySelector("#searchValue").value;
+  const newArray = Object.entries(carsObj).filter(function (current) {
+    return current[1][`${fixtype}`].toString() === innerType.toString();
+  })
+  console.log(newArray)
+  return newArray
 }
 
 init();
