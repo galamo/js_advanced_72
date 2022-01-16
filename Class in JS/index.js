d1b52cade6125f60ba6e8afa9abf6af039c7dc1e
galamo <galamouyal88@gmail.com>
2022-01-16 18:56:56 +0200

    classes exercise

--- Class in JS/index.js
+++ Class in JS/index.js
@@ -18,6 +18,7 @@ class Warrior {
         this.id = Math.ceil(Math.random() * 9999)
         this.level = level
         this.name = name
+        this.armorLevel = 0;
     }
     attack() {
         console.log(`${this.name} attacks with level ${this.level}`)
@@ -25,12 +26,47 @@ class Warrior {
 }
 
 class Archer extends Warrior {
-    constructor(numberOfArrows, name, level) {
+    constructor(name, level) {
         super(name, level)
-        this.numberOfArrows = numberOfArrows;
+        this.numberOfArrows = 0;
         this.bowLevel = 0
+    }
+
+    attack() {
+        if (this.numberOfArrows <= 0) return;
+        this.numberOfArrows = this.numberOfArrows - 5
+        console.log(`Archer ${this.name} attacks with bow level ${this.bowLevel}`)
+    }
+
+    setNumberOfArrows(value) {
+        if (typeof value !== "number") throw new Error("You must insert Number ");
+        this.numberOfArrows = value
+    }
+}
+
+class SwordMan extends Warrior {
+    constructor(name, level) {
+        super(name, level)
+        this.swordLevel = 0;
+    }
+    attack() {
+        console.log(`Sword man ${this.name} attacks with swordLevel ${this.swordLevel}`)
     }
 }
 
 const warrior1 = new Warrior("warrior1", 10)
-const archer1 = new Archer(50, "archer1", 10)
+const archer1 = new Archer("archer1", 10, 50)
+const swordman1 = new SwordMan("swordMan1", 50)
+// warrior1.attack()
+// archer1.attack()
+// swordman1.attack()a
+
+const warrios = [warrior1, archer1, swordman1]
+function startWar() {
+    warrios.forEach(warrior => { warrior.attack() })
+}
+
+
+const testwarrior = new Archer();
+
+const archerCopy = new Archer(testwarrior.name, testwarrior.numberOfArrows, testwarrior.level)
--- Class in JS/readme.md
+++ Class in JS/readme.md
@@ -1,3 +1,9 @@
 # Class
 1. Create SwordMan class with sword level + armor level
 2. create an object based on the swordMan class `new SwordMan`
+
+# Class Summarise
+1. Class AnimalBase ( name , color, AnotherCommonProp) - (commonBhevaior)) 1 
+2. Class AnimalClass - 3 (AnotherCommonProp) (commonBhevaior)
+3. Class Dog, Bird, Fish   - 3  ( AnotherCommonProp ) (explicit behavior)
+
