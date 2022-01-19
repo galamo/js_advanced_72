0f199f1cc484402ae43f3ba02802a55bd3085ec6
galamo <galamouyal88@gmail.com>
2022-01-19 19:15:47 +0200

    fetch create fruit

--- fruitsClient/index.js
+++ fruitsClient/index.js
@@ -8,15 +8,31 @@ $(document).ready(init)
 function init() {
 
     $("#searchButton").on("click", function () {
-        const searchInputValue = $("#searchInput").val()
-        $("#content").append(`<h2>${searchInputValue}</h2>`)
-
+        addFruit()
     })
+
+
 
     getAllFruits()
 
 }
+function addFruit() {
+    const fruitsUrl = "fruit"
+    const currentFruit = $("#searchInput").val()
+    if (!currentFruit) return;
+    fetch(`${ApiBaseUrl}/${fruitsUrl}/${currentFruit}`, {}).then(_setJson).then(_addFruitSuccess).catch(_setError)
+    function _setJson(response) {
+        return response.json()
+    }
 
+    function _addFruitSuccess(message) {
+        popUpSuccessModal()
+        getAllFruits()
+    }
+    function _setError(ex) {
+        console.log(ex)
+    }
+}
 function getAllFruits() {
     const fruitsUrl = "fruits"
     fetch(`${ApiBaseUrl}/${fruitsUrl}`).then(_setJson).then(_loadFruits).catch(_setError)
@@ -35,3 +51,15 @@ function getAllFruits() {
 
 }
 
+function popUpSuccessModal() {
+    $("#alertModalSuccess").css({ visibility: "visible" })
+    setTimeout(function () {
+        $("#alertModalSuccess").css({ visibility: "hidden" })
+    }, 5000);
+}
+
+function printMe(str, options = {}) {
+    const isUpper = options.upper
+    const whatToPrint = isUpper ? str.toUpperCase() : str
+    console.log(whatToPrint)
+}
