88fbc52a3dd771d8e79153c0967c71961b74394c
galamo <galamouyal88@gmail.com>
2022-01-19 18:33:29 +0200

    body

--- restApi/index.js
+++ restApi/index.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const app = express();
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 
 const talsFruits = ["coconut", "orange", "mango", "peanaple"]
 let num = 0;
@@ -25,6 +30,11 @@ app.get("/fruit/:fruit", (req, res, next) => {
     console.log(req.params)
     talsFruits.push(req.params.fruit)
     res.json({ message: "success!" })
+})
+app.post("/fruit", (req, res, next) => {
+    console.log(req.body)
+    talsFruits.push(req.body.fruit)
+    res.json({ message: "POST request Fruit created" })
 })
 
 
